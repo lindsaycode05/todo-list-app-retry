ed736664bde49f9e261af820fab409ddfd0a2eaf
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-02 19:54:28 +0200

    Not finished: Set completed value to true when task is checked

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,15 @@ input.addEventListener('keyup', (e) => {
 });
 
 let tasks = [];
-let counter = 0;
+// let counter = 0;
+let dataId = 1;
+
+tasks.push({
+    id: tasks.length + 1,
+    task: input.value,
+    done: false,
+  });
+  console.log(tasks);
 
 function renderTask() {
   const item = document.createElement('div');
@@ -21,6 +29,7 @@ function renderTask() {
   const checkButton = document.createElement('button');
   const task = document.createElement('p');
   const taskValue = (task.innerText = input.value);
+  task.setAttribute('data-taskid', `${dataId++}`);
   itemContainer.append(checkButton, task);
   const deleteButton = document.createElement('button');
   const deleteButtonCross = document.createElement('img');
@@ -28,11 +37,7 @@ function renderTask() {
   deleteButton.appendChild(deleteButtonCross);
   item.append(itemContainer, deleteButton);
   listContainer.prepend(item);
-
-  tasks.push({
-    task: taskValue,
-    done: false,
-  });
+ 
 
   completeTask();
 }
@@ -41,8 +46,31 @@ function completeTask() {
   const checkButton = document.querySelector('.todo__list-tasks-task button');
   const task = document.querySelector('.todo__list-tasks-task p');
 
-  checkButton.addEventListener('click', () => {
+  checkButton.addEventListener('click', (e) => {
     checkButton.classList.toggle('completed-task');
-    task.classList.toggle('line-through')
+    task.classList.toggle('line-through');
+    // const taskId = parseInt(task.dataset.taskId)
+    // console.log(taskId);
+    const matchedTask = tasks.find(item => String(task.id) === task.dataset.taskid)
+    console.log(matchedTask);
+    // matchedTask.done = true;
+    // const ex = tasks.findIndex(i => i.id === 3)
+    // console.log(ex);
+    // const checkIfDone = 
+    // console.log(tasks.map(item => item[taskId].id))
+    // console.log(tasks);
+    // checkIfDone.done = true
+    // console.log(tasks);
+
+    // const (checkButton.nextElementSibling);
+    // const taskDataId =
+    // console.log(e.target.nextElementSibling.dataset.taskid)
+    // const checkCompleted =
+    // console.log(tasks.find(task => task.id === taskDataId))
+    // checkCompleted.done = true;
+    // console.log(tasks);
   });
 }
+
+
+
